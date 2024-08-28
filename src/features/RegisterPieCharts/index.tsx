d133bf3c5008b6a8713f5d67a630f68a4df6a242
Rafael Lino <88785684+rafael-lino@users.noreply.github.com>
2024-08-27 21:51:06 -0300

    Feature/add calculator (#16)
    * start feature
    
    * update calculator
    
    * add pie charts
    
    * add profile picture
    
    * fix
    ---------
    
    Co-authored-by: Rafael Lino <rafael.linosp@gmail.com>

---
+++ src/features/RegisterPieCharts/index.tsx
@@ -0,0 +1,45 @@
+import { PizzaChart } from '@/components/PizzaChart';
+import { formatCurrency } from '@/lib/utils';
+import { useExpensesBalance, useIncomesBalance, useInvestmentsBalance, useRegisters } from '@/store/store';
+import { TrendingUp } from 'lucide-react';
+import { useMemo } from 'react';
+
+export function IncomesPieChart() {
+  const [incomes] = useRegisters('incomes');
+  const filtered = useMemo(() => incomes.filter((item) => item.checked), [incomes]);
+  const balance = useIncomesBalance();
+  const remain = balance.planned - balance.done;
+  const data = filtered.length === 0 ? incomes : filtered;
+  return (
+    <PizzaChart key='incomes' data={data}>
+      Remaining {formatCurrency(remain)}
+      <TrendingUp className='h-4 w-4 mx-2' />
+    </PizzaChart>
+  );
+}
+
+export function ExpensesPieChart() {
+  const [expenses] = useRegisters('expenses');
+  const filtered = useMemo(() => expenses.filter((item) => item.checked), [expenses]);
+  const balance = useExpensesBalance();
+  const remain = balance.planned - balance.done;
+  const data = filtered.length === 0 ? expenses : filtered;
+  return (
+    <PizzaChart key='expenses' data={data}>
+      Remaining {formatCurrency(remain)} <TrendingUp className='h-4 w-4 mx-2' />
+    </PizzaChart>
+  );
+}
+
+export function InvestmentsPieChart() {
+  const [investments] = useRegisters('investments');
+  const filtered = useMemo(() => investments.filter((item) => item.checked), [investments]);
+  const balance = useInvestmentsBalance();
+  const remain = balance.planned - balance.done;
+  const data = filtered.length === 0 ? investments : filtered;
+  return (
+    <PizzaChart key='investments' data={data}>
+      Remaining {formatCurrency(remain)} <TrendingUp className='h-4 w-4 mx-2' />
+    </PizzaChart>
+  );
+}
--- src/features/UserMenu/index.tsx
+++ src/features/UserMenu/index.tsx
@@ -13,7 +13,7 @@ import UserImg from '@/assets/placeholder-user.jpg';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useState } from 'react';
 export const UserMenu = () => {
-  const { logout } = useAuth0();
+  const { logout, user } = useAuth0();
   const [loading, setLoading] = useState(false);
 
   const handleLogout = async () => {
@@ -28,7 +28,7 @@ export const UserMenu = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon' className='overflow-hidden rounded-full'>
-          <img src={UserImg} width={36} height={36} alt='Avatar' className='overflow-hidden rounded-full' />
+          <img src={user?.picture ?? UserImg} width={36} height={36} alt='Avatar' className='overflow-hidden rounded-full' />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
