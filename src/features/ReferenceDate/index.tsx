c0fda7b7d60eecc6bb35f75c3dd5ff09fa666670
rafael-lino <rafael.lino@ciandt.com>
2024-09-28 19:38:24 -0300

    fix

--- src/features/ReferenceDate/index.tsx
+++ src/features/ReferenceDate/index.tsx
@@ -12,17 +12,21 @@ const nextMonth = isAfterClosingDay && new Date(NOW).setMonth(NOW.getMonth() + 1
 export const ReferenceDate = memo(() => {
   const { t } = useTranslation();
   return (
-    <div className="flex justify-center w-full sm:gap-1">
-      <h1 className={cn('sm:text-2xl font-semibold whitespace-nowrap text-zinc-600', isAfterClosingDay && 'opacity-60')}>
-        <span className="hidden sm:block">{t('date', { date: NOW })}</span>
-        <span className="block sm:hidden">{t('date', { date: NOW, context: { format: 'MMMM' } })}</span>
+    <div className='flex justify-center w-full sm:gap-1'>
+      <h1
+        className={cn('sm:text-2xl font-semibold whitespace-nowrap text-zinc-600', isAfterClosingDay && 'opacity-60')}
+      >
+        <span className='hidden sm:block'>{t('date', { date: NOW })}</span>
+        <span className='block sm:hidden'>
+          {t('date', { date: NOW, context: { format: isAfterClosingDay ? 'MMM' : 'MMMM' } })}
+        </span>
       </h1>
       {isAfterClosingDay && (
-        <div className="flex items-center">
-          <MoveRight className="h-4 w-4 mx-2" />
-          <h1 className="sm:text-2xl font-semibold whitespace-nowrap text-red-500">
-            <span className="hidden sm:block">{t('date', { date: nextMonth })}</span>
-            <span className="block sm:hidden">{t('date', { date: nextMonth, context: { format: 'MMM' } })}</span>
+        <div className='flex items-center'>
+          <MoveRight className='h-4 w-4 mx-2' />
+          <h1 className='sm:text-2xl font-semibold whitespace-nowrap text-red-500'>
+            <span className='hidden sm:block'>{t('date', { date: nextMonth })}</span>
+            <span className='block sm:hidden'>{t('date', { date: nextMonth, context: { format: 'MMM' } })}</span>
           </h1>
         </div>
       )}
