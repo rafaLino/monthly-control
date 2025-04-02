d8a2253407137a3cc2ecaf6cd3a81dba733f141d
Rafa Lino <rafael.linosp@gmail.com>
2025-04-01 21:39:51 -0300

    remove feature flag component

--- src/routes/_main/settings.lazy.tsx
+++ src/routes/_main/settings.lazy.tsx
@@ -1,4 +1,3 @@
-import { FeatureFlags } from '@/features/FeatureFlags';
 import { SetClosingDay } from '@/features/SetClosingDay';
 import { SetGoals } from '@/features/SetGoals';
 import { createLazyFileRoute } from '@tanstack/react-router';
@@ -11,9 +10,7 @@ function Index() {
     <div className="flex w-full flex-col">
       <main className="flex flex-1 flex-col gap-4 px-4 sm:p-4 md:gap-4 md:p-6">
         <div className="mx-auto grid w-full items-start gap-6 md:grid-cols-[180px_1fr] 2xl:grid-cols-[300px_1fr]">
-          <div>
-            <FeatureFlags />
-          </div>
+          <div />
           <div className="grid gap-6 w-full md:max-w-3xl 2xl:max-w-5xl">
             <SetGoals />
             <SetClosingDay />
