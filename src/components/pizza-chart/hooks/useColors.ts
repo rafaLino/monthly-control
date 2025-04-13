1c97e9f121b300be094a4ba50ffae0bbc565e786
Rafa Lino <rafael.linosp@gmail.com>
2025-04-13 12:41:15 -0300

    refactor useColors

--- src/components/pizza-chart/hooks/useColors.ts
+++ src/components/pizza-chart/hooks/useColors.ts
@@ -1,15 +1,20 @@
+import { usePrevious } from '@/hooks/usePrevious';
 import randomColor from 'randomcolor';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useColors(count: number) {
-  const colorsRef = useRef(randomColor({ count, format: 'hsl', luminosity: 'bright' }));
-  const countRef = useRef(count);
-
+  const [colors, setColors] = useState(randomColor({ count, format: 'hsl', luminosity: 'bright' }));
+  const prevCount = usePrevious(count, 0)
   useEffect(() => {
-    if (colorsRef.current && countRef.current !== count) {
-      colorsRef.current = colorsRef.current.concat(randomColor({ format: 'hsl', luminosity: 'bright' }));
-    }
-  }, [count]);
+    if (prevCount > 0) {
+      setColors((prev) => {
+        if (count > prevCount)
+          return [...prev, randomColor({ format: 'hsl', luminosity: 'bright' })]
 
-  return colorsRef.current;
+        return prev.toSpliced(prev.length - 1);
+      })
+    }
+  }, [count, prevCount]);
+
+  return colors;
 }
