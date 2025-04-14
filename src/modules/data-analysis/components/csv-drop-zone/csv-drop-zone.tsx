660de44aded4f02b823dbe2732abd2e3404c9aac
Rafael Lino <88785684+rafael-lino@users.noreply.github.com>
2025-04-14 19:41:35 -0300

    Fix/layout (#55)
    * fix small issue layout
    
    * 2.1.1

--- src/modules/data-analysis/components/csv-drop-zone/csv-drop-zone.tsx
+++ src/modules/data-analysis/components/csv-drop-zone/csv-drop-zone.tsx
@@ -13,7 +13,7 @@ export const CsvDropZone: FC<Props> = ({ onDrop }) => {
   return (
     <Dropzone onDrop={handleDrop} accept={{ 'text/*': ['.csv'] }} maxFiles={1}>
       {({ getRootProps, getInputProps }) => (
-        <section className="flex w-4/5 mt-12 min-h-96 items-center justify-center border-2 border-dashed rounded-lg border-gray-500 text-gray-800 bg-slate-100/50 ">
+        <section className="flex w-4/5 mt-12 min-h-96 items-center p-2 text-center justify-center border-2 border-dashed rounded-lg border-gray-500 text-gray-800 bg-slate-100/50 ">
           <div {...getRootProps()}>
             <input {...getInputProps()} />
             <span>
