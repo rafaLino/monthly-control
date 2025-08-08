11e8133e0135ede7d6043e8750cdb541622a1ff4
Rafael Lino <rafael.linosp@gmail.com>
2025-08-07 22:15:13 -0300

    feat: add clear button

--- src/modules/planner/features/projection-dialog/components/projection-content.tsx
+++ src/modules/planner/features/projection-dialog/components/projection-content.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useKeyDown } from '@/hooks/useKeyDown';
 import { sum } from '@/lib/utils';
 import { ProjectionTable } from '@/modules/planner/features/projection-dialog/components/projection-table';
 import { getAll } from '@/store';
 import { RegisterType } from '@/types/register.types';
-import { useMemo, useReducer, useRef } from 'react';
+import { useMemo, useReducer } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useFilterInput } from '../hooks/useFilterInput';
 import { reducer } from '../utils/projection-reducer';
@@ -13,11 +14,10 @@ import { ProjectionResultCard } from './projection-result-card';
 export const ProjectionContent = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'projectionDialog' });
   const [state, dispatch] = useReducer(reducer, getAll(), getAll);
-  const inputRef = useRef<HTMLInputElement>(null);
   const [data, filterValue, onFilter] = useFilterInput(state);
 
   useKeyDown('Escape', () => {
-    inputRef.current?.focus();
+    dispatch({ action: 'clear' });
   });
 
   const incomes = useMemo(() => sum(state.incomes.filter((i) => i.checked)), [state.incomes]);
@@ -33,17 +33,27 @@ export const ProjectionContent = () => {
     dispatch({ id, type });
   };
 
+  const handleClear = () => {
+    dispatch({ action: 'clear' });
+  };
+
   return (
     <>
-      <div className="flex flex-row items-center flex-wrap justify-evenly gap-1 w-full sm:w-1/2 sm:justify-self-center mt-8 sm:m-0">
+      <div className="flex flex-row items-center justify-evenly gap-1 w-full sm:w-1/2 sm:justify-self-center mt-8 sm:m-0">
         <ProjectionResultCard value={incomes} variant="success" />
         <p className="font-medium">-</p>
         <ProjectionResultCard value={costs} variant="warning" />
         <p className="font-medium">=</p>
         <ProjectionResultCard value={result} error={result < 0} />
       </div>
-      <div className="flex items-center sm:justify-self-center sm:w-1/3 ">
-        <Input ref={inputRef} tabIndex={-1} placeholder={t('search')} value={filterValue} onChange={onFilter} />
+      <div className="flex items-center justify-between gap-1 sm:gap-2">
+        <Button tabIndex={-1} variant="secondary" className="border hover:bg-gray-200" onClick={handleClear}>
+          {t('clear')}
+        </Button>
+        <div className="flex items-center w-full sm:w-1/3">
+          <Input tabIndex={-1} placeholder={t('search')} value={filterValue} onChange={onFilter} />
+        </div>
+        <div />
       </div>
       <section className="flex flex-col sm:flex-row items-start justify-between gap-2">
         <ProjectionTable key="incomes" records={data.incomes} type="incomes" onCheck={handleCheck} />
--- src/modules/planner/features/projection-dialog/components/projection-result-card.tsx
+++ src/modules/planner/features/projection-dialog/components/projection-result-card.tsx
@@ -22,7 +22,7 @@ function getColorClass(variant: string) {
 export const ProjectionResultCard: FC<ProjectionResultCardProps> = ({ value, error = false, variant = 'info' }) => {
   const { bgColor, textColor } = getColorClass(variant);
   return (
-    <Card className={cn('text-center text-md p-2 min-w-28 sm:min-w-32', bgColor)}>
+    <Card className={cn('text-center text-md p-2 sm:min-w-32', bgColor)}>
       <Translation>
         {(t) => <CardTitle className={cn(textColor, error && 'text-red-500')}>{t('currency', { value })}</CardTitle>}
       </Translation>
