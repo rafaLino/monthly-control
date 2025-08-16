2b7b1a23385d2d5100d9430da5588f5db9c93d42
Rafael Lino <rafael.linosp@gmail.com>
2025-08-16 04:33:53 -0300

    fix: move translation hook up

--- src/modules/data-analysis/features/assistant/components/chat/index.tsx
+++ src/modules/data-analysis/features/assistant/components/chat/index.tsx
@@ -1,9 +1,11 @@
 import { useMessages } from '@/store';
+import { useTranslation } from 'react-i18next';
 import { useAssistant } from '../hooks/useAssistant';
 import { MessageBox } from './message-box';
 import { MessageInput } from './message-input';
 
 export const Chat = () => {
+  const { t } = useTranslation('translation', { keyPrefix: 'assistant' });
   const { loading, withContext, addMessage, setWithContext } = useAssistant();
   const messages = useMessages();
 
@@ -11,6 +13,7 @@ export const Chat = () => {
     <div className="w-full h-full overflow-auto flex flex-col border rounded-lg shadow-md p-2 pb-0 bg-slate-100 dark:bg-slate-700 text-zinc-500">
       <MessageBox messages={messages} />
       <MessageInput
+        placeholder={t('inputPlaceholder')}
         loading={loading}
         attachmentEnabled={withContext}
         onInputValue={addMessage}
--- src/modules/data-analysis/features/assistant/components/chat/message-input.tsx
+++ src/modules/data-analysis/features/assistant/components/chat/message-input.tsx
@@ -3,52 +3,53 @@ import { Textarea } from '@/components/ui/textarea';
 import { Toggle } from '@/components/ui/toggle';
 import { Loader2Icon, Paperclip } from 'lucide-react';
 import { FC, KeyboardEvent, memo, useRef } from 'react';
-import { useTranslation } from 'react-i18next';
 
 type MessageInputProps = {
   onInputValue: (value: string) => void;
   onAttachmentChange?: (pressed: boolean) => void;
   attachmentEnabled?: boolean;
   loading?: boolean;
+  placeholder?: string;
 };
-export const MessageInput: FC<MessageInputProps> = memo(({ onInputValue, onAttachmentChange, attachmentEnabled, loading }) => {
-  const { t } = useTranslation('translation', { keyPrefix: 'assistant' });
-  const inputRef = useRef<HTMLTextAreaElement>(null);
+export const MessageInput: FC<MessageInputProps> = memo(
+  ({ onInputValue, onAttachmentChange, attachmentEnabled, loading, placeholder }) => {
+    const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault();
-      if (inputRef.current) {
-        onInputValue(inputRef.current.value);
-        inputRef.current.value = '';
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        if (inputRef.current) {
+          onInputValue(inputRef.current.value);
+          inputRef.current.value = '';
+        }
       }
-    }
-  };
+    };
 
-  return (
-    <div className="my-2">
-      <Textarea
-        ref={inputRef}
-        disabled={loading}
-        placeholder={t('inputPlaceholder')}
-        className="resize-none min-h-5 h-12 text-zinc-500 dark:text-zinc-100"
-        onKeyDown={handleKeyDown}
-      />
+    return (
+      <div className="my-2">
+        <Textarea
+          ref={inputRef}
+          disabled={loading}
+          placeholder={placeholder}
+          className="resize-none min-h-5 h-12 text-zinc-500 dark:text-zinc-100"
+          onKeyDown={handleKeyDown}
+        />
 
-      {loading ? (
-        <Button size="icon" variant="link" className="absolute right-3 bottom-3.5">
-          <Loader2Icon className="h-4 w-4 animate-spin" />
-        </Button>
-      ) : (
-        <Toggle
-          aria-label="Toggle attachments"
-          pressed={attachmentEnabled}
-          className="absolute right-3 bottom-3.5"
-          onPressedChange={onAttachmentChange}
-        >
-          <Paperclip className="h-4 w-4" />
-        </Toggle>
-      )}
-    </div>
-  );
-});
+        {loading ? (
+          <Button size="icon" variant="link" className="absolute right-3 bottom-3.5">
+            <Loader2Icon className="h-4 w-4 animate-spin" />
+          </Button>
+        ) : (
+          <Toggle
+            aria-label="Toggle attachments"
+            pressed={attachmentEnabled}
+            className="absolute right-3 bottom-3.5"
+            onPressedChange={onAttachmentChange}
+          >
+            <Paperclip className="h-4 w-4" />
+          </Toggle>
+        )}
+      </div>
+    );
+  }
+);
