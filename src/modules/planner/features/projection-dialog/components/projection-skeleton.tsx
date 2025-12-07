985ab69595dd0dee43cf9428727e6f3a616e684f
Rafael Lino <rafael.linosp@gmail.com>
2025-12-07 19:13:23 -0300

    fix: blunde size

---
+++ src/modules/planner/features/projection-dialog/components/projection-skeleton.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+export const ProjectionSkeleton = () => {
+    return (
+        <div className="grid grid-cols-4 gap-4 py-4">
+            <Skeleton className="h-full w-full" />
+            <Skeleton className="h-full w-full" />
+            <Skeleton className="h-full w-full" />
+            <Skeleton className="h-full w-full" />
+        </div>
+    )
+}
