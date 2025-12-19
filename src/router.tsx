6c6432432e6112645ec58e81200a386d33e2f44b
Rafael Lino <rafael.lino@ciandt.com>
2025-12-19 11:11:12 -0300

    not found page

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,17 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
 import { routeTree } from './routeTree.gen';
+import { NotFound } from './layouts/not-found';
 
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router;
   }
 }
-const router = createRouter({ routeTree });
+const router = createRouter({ 
+  routeTree,
+  defaultNotFoundComponent: NotFound
+});
 
 export function AppRoute() {
   return <RouterProvider router={router} />;
