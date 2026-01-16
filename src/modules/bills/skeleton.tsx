4bfc467a58c549137c4462cb6fe2dd48cf02c515
Rafael Lino <rafael.linosp@gmail.com>
2026-01-15 22:23:43 -0300

    fix layout

--- src/modules/bills/skeleton.tsx
+++ src/modules/bills/skeleton.tsx
@@ -2,11 +2,11 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export const BillsModuleSkeleton = () => {
   return (
-    <main className="grid grid-rows-1 grid-cols-1 h-full gap-4 p-2 sm:grid-cols-3 sm:grid-rows-3 sm:px-4 max-h-(--max-h-height)">
-      <Skeleton className="h-80 sm:h-210 p-2 row-span-1 sm:row-span-3 overflow-hidden flex flex-col" />
-      <Skeleton className="p-2 h-50 sm:h-80" />
-      <Skeleton className="p-2 h-50 sm:h-80" />
-      <Skeleton className="p-2 h-50 sm:h-125 col-span-1 row-span-1 sm:row-span-2 sm:col-span-2" />
+    <main className="grid grid-rows-1 grid-cols-1 h-full gap-4 p-2 sm:grid-cols-3 sm:grid-rows-3 sm:px-4 max-h-(--max-h-lg)">
+      <Skeleton className="h-80 sm:h-205 p-2 row-span-1 sm:row-span-3 overflow-hidden flex flex-col" />
+      <Skeleton className="p-2 " />
+      <Skeleton className="p-2 " />
+      <Skeleton className="p-2  col-span-1 row-span-1 sm:row-span-2 sm:col-span-2" />
     </main>
   );
 };
