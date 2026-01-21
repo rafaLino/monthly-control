6f3e4e3a74207341abeb97d3fe30a1d679ca228e
Rafael Lino <rafael.lino@ciandt.com>
2026-01-21 18:19:19 -0300

    fix lint

--- src/components/pizza-chart/hooks/useColors.ts
+++ src/components/pizza-chart/hooks/useColors.ts
@@ -5,15 +5,15 @@ const generateColors = (previous: string[], count: number) => {
   if (previous.length === 0) {
     return randomColor({ count, format: 'hsl', luminosity: 'bright' });
   }
-  return count >= previous.length ? [...previous, randomColor({ format: 'hsl', luminosity: 'bright' })] : previous.toSpliced(-1)
-}
+  return count >= previous.length ? [...previous, randomColor({ format: 'hsl', luminosity: 'bright' })] : previous.toSpliced(-1);
+};
 
 export function useColors(count: number) {
   const [colors, setColors] = useState<string[]>([]);
 
   useEffect(() => {
     startTransition(() => {
-      setColors(prev => generateColors(prev, count));
+      setColors((prev) => generateColors(prev, count));
     });
   }, [count]);
 
